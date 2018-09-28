9dc5f34a435749ed521caa2a13f820d08f153617
James Sutherland <39761427+LondonJim@users.noreply.github.com>
2018-09-28 17:07:24 +0100

    Update seeds.rb
    Delete seeding of table. Not needed anymore.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-User.create(full_name: "Test Name", email: "abc@def.com", encrypted_password: "123456")
