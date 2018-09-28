0521a1b5e0dac1764b4aca8dd5cd4b82b99e68f1
James Sutherland <jrsutherland78@googlemail.com>
2018-09-28 10:20:33 +0100

    refactor: removed redundant user validation and model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,4 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
-  validates :full_name, presence: true, length: { maximum: 50 }
-  validates :handle, presence: true, length: { maximum: 50 }, uniqueness: true
-  validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 6, maximum: 10 }
-
 end
