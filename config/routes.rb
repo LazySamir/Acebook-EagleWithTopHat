dde1266a4fbd916bd3aebddc93ec3be01ad28ee5
Alastair Edmonds <alastair.edmonds@gmail.com>
2018-10-03 11:35:18 +0100

    update applicationController, created registrationController so it can respond to JSON

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
   
-  # devise_for :users
+  devise_for  :users,
+              controllers: {
+                sessions: 'sessions',
+                registrations: 'registrations'
+              }
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts 
-  # do
-  #   # resources :comments
-  #   # resources :likes
-  # end
+  do
+    resources :comments
+    resources :likes
+  end
 
   root 'posts#index'
 
