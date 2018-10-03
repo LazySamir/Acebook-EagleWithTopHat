3eb369463a7be5cd6df7002222fbc0a435c4f104
Alastair Edmonds <alastair.edmonds@gmail.com>
2018-10-03 14:16:45 +0100

    add second passing test for checking invalid user params

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
           :registerable,
           :recoverable,
           :rememberable,
-          # :validatable,
+          :validatable,
           :jwt_authenticatable,
           jwt_revocation_strategy: JWTBlacklist
 end
