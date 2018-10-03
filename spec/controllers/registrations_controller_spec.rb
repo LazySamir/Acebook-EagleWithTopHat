5c845789fdfec1005c0f7f8f48699105abf81b11
James Sutherland <jrsutherland78@googlemail.com>
2018-10-03 17:28:13 +0100

    test: registrations_controller post tests all passing

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -1,10 +1,10 @@
-require 'rails_helper' 
+require 'rails_helper'
 
 RSpec.describe RegistrationsController, type: :controller do
 
-  describe "Post /" do 
-    let(:params) do 
-      { 
+  describe "Post /" do
+    let(:params) do
+      {
         'user': {
           'full_name': 'Test Name',
           'email': 'test@email.com',
@@ -13,7 +13,7 @@ RSpec.describe RegistrationsController, type: :controller do
         }
       }
     end
-    let(:invalid_params) do 
+    let(:invalid_params) do
       {
         'user': {
           'full_name': 'Not Invalid Name',
@@ -24,32 +24,28 @@ RSpec.describe RegistrationsController, type: :controller do
       }
     end
 
-    before do 
+    before do
       @request.env["devise.mapping"] = Devise.mappings[:user]
-    end  
+    end
 
     context 'with valid params' do
        before { post :create, params: params  }
-      it 'returns status code 200' do 
-        post :create, params: params 
+      it 'returns status code 200' do
         expect(response.status).to eq 200
       end
       it 'returns json data' do
-        post :create, params: params 
         expect(response.content_type).to eq('application/json')
       end
-      it 'provides a success indication' do 
-        post :create, params: params 
+      it 'provides a success indication' do
         expect(response).to be_successful
       end
-    
+
     end
 
-    context 'with invalid params' do 
-      it 'it does not create a new user' do 
+    context 'with invalid params' do
+      it 'it does not create a new user' do
         post :create, params: invalid_params
         expect(response.status).to eq 400
-        expect(response).to be_successful
         expect(response.content_type).to eq('application/json')
       end
     end
